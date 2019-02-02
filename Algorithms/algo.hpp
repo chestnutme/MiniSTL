44bbd03d4f03f5e470b7f73764d4be87971c1965
chestnutme <1724759858@qq.com>
2019-02-02 23:04:24 +0800

    add reverse, remove, rorate, generate

--- Algorithms/algo.hpp
+++ Algorithms/algo.hpp
@@ -162,7 +162,7 @@ BiIt1 __find_end(BiIt1 first1, BiIt1 last1,
                  BiIt2 first2, BiIt2 last2,
                  bidirectional_iterator_tag,
                  bidirectional_iterator_tag, 
-                 BiPredicate comp = BiPrecidate()) {
+                 BiPredicate comp = BiPredicate()) {
     using ReIt1 = __reverse_iterator<BiIt1>;
     using ReIt2 = __reverse_iterator<BiIt2>;
     ReIt1 rlast1(first1);
@@ -188,8 +188,8 @@ find_end(ForwardIt1 first1, ForwardIt1 last1,
          ForwardIt2 first2, ForwardIt2 last2,
          BiPredicate comp) {
   return find_end(first1, last1, first2, last2,
-                  iterator_category<ForwardIt1>(),
-                  iterator_category<ForwardIt2>(),
+                  iterator_category_t<ForwardIt1>(),
+                  iterator_category_t<ForwardIt2>(),
                   comp);
 }
 
@@ -335,11 +335,11 @@ inline ForwardIt upper_bound(ForwardIt first, ForwardIt last,
     difference_type_t<ForwardIt> half;
     ForwardIt mid;
 
-    while (len > 0) {
+    while(len > 0) {
         half = len >> 1;
         mid = first;
         advance(mid, half);
-        if (comp(val, *mid))
+        if(comp(val, *mid))
             len = half;
         else {
             first = mid;
@@ -360,16 +360,15 @@ equal_range(ForwardIt first, ForwardIt last, const T& val,
     difference_type_t<ForwardIt> half;
     ForwardIt mid, left, right;
 
-    while (len > 0) {
+    while(len > 0) {
         half = len >> 1;
         mid = first;
         advance(mid, half);
-        if (comp(*mid, val)) {
+        if(comp(*mid, val)) {
             first = mid;
             ++first;
             len = len - half - 1;
-        }
-        else if (comp(val, *mid))
+        } else if(comp(val, *mid))
             len = half;
         else {
             left = lower_bound(first, mid, val, comp);
@@ -388,6 +387,310 @@ bool binary_search(ForwardIt first, ForwardIt last,
                    Compare comp = Compare()) {
     ForwardIt i = lower_bound(first, last, val, comp);
     return i != last && !comp(val, *i);
+}
+
+// replace, replace_if, replace_copy, replace_copy_if
+template <class ForwardIt, class T>
+void replace(ForwardIt first, ForwardIt last,
+             const T& old_val, const T& new_val) {
+    for(;first != last; ++first) {
+        if(*first == old_val)
+            *first = new_val;
+    }
+}
+
+template <class ForwardIt, class Predicate, class T>
+void replace_if(ForwardIt first, ForwardIt last,
+                Predicate pred, const T& new_val) {
+    for(;first != last; ++first) {
+        if(pred(*first))
+            *first = new_val;
+    }
+}
+
+template <class InputIt, class OutputIt, class T>
+OutputIt replace_copy(InputIt first, InputIt last,
+                      OutputIt result, const T& old_val, 
+                      const T& new_val) {
+    for(;first != last; ++first, ++result)
+        *result = *first == old_val ? new_val : *first;
+    return result;
+}
+
+template <class InputIt, class OutputIt, class Predicate, class T>
+OutputIt replace_copy_if(InputIt first, InputIt last,
+                         OutputIt result,
+                         Predicate pred, const T& new_val) {
+    for(;first != last; ++first, ++result)
+        *result = pred(*first) ? new_val : *first;
+    return result;
+}
+
+// remove, remove_if, remove_copy, remove_copy_if
+template <class ForwardIt, class T>
+ForwardIt remove(ForwardIt first, ForwardIt last,
+                 const T& val) {
+    first = find(first, last, val);
+    ForwardIt i = first;
+    return first == last ? first 
+                        : remove_copy(++i, last, first, val);
+}
+
+template <class ForwardIt, class Predicate>
+ForwardIt remove_if(ForwardIt first, ForwardIt last,
+                    Predicate pred) {
+    first = find_if(first, last, pred);
+    ForwardIt i = first;
+    return first == last ? first 
+                        : remove_copy_if(++i, last, first, pred);
+}
+
+template <class InputIt, class OutputIt, class T>
+OutputIt remove_copy(InputIt first, InputIt last,
+                     OutputIt result, const T& val) {
+    for(;first != last; ++first) {
+        if(!(*first == val)) {
+            *result = *first;
+            ++result;
+        }
+    }
+    return result;
+}
+
+template <class InputIt, class OutputIt, class Predicate>
+OutputIt remove_copy_if(InputIt first, InputIt last,
+                        OutputIt result, Predicate pred) {
+    for(;first != last; ++first) {
+        if(!pred(*first)) {
+            *result = *first;
+            ++result;
+        }
+    }
+    return result;
+}
+
+// unique and unique_copy
+template <class InputIt, class OutputIt, class BiPredicate,
+          class T>
+OutputIt unique_copy(InputIt first, InputIt last,
+                     OutputIt result,
+                     BiPredicate pred) {
+    value_type_t<InputIt> val = *first;
+    *result = val;
+    while(++first != last) {
+        if(!pred(val, *first)) {
+            val = *first;
+            *++result = val;
+        }
+    }
+    return ++result;
+}
+
+template <class InputIt, class OutputIt, class BiPredicate>
+inline OutputIt unique_copy(InputIt first, InputIt last,
+                            OutputIt result,
+                            BiPredicate pred,
+                            output_iterator_tag) {
+    return unique_copy(first, last, result, pred);
+}
+
+template <class InputIt, class ForwardIt, class BiPredicate>
+ForwardIt unique_copy(InputIt first, InputIt last,
+                      ForwardIt result, 
+                      BiPredicate pred,
+                      forward_iterator_tag) {
+    *result = *first;
+    while(++first != last) {
+        if(!pred(*result, *first)) 
+            *++result = *first;
+    }
+    return ++result;
+}
+
+template <class InputIt, class OutputIt, 
+          class BiPredicate = equal_to<value_type_t<InputIt>> >
+inline OutputIt unique_copy(InputIt first, InputIt last,
+                            OutputIt result,
+                            BiPredicate pred = BiPredicate()) {
+    if(first == last)
+        return result;
+    return unique_copy(first, last, result, pred,
+                       iterator_category_t<InputIt>());
+}
+
+template <class ForwardIt, 
+          class BiPredicate = equal_to<value_type_t<ForwardIt>> >
+ForwardIt unique(ForwardIt first, ForwardIt last,
+                    BiPredicate pred) {
+    first = adjacent_find(first, last, pred);
+    return unique_copy(first, last, first, pred);
+}
+
+
+//------------------------
+// reverse and reverse_copy
+template <class BiIt>
+void __reverse(BiIt first, BiIt last, 
+               bidirectional_iterator_tag) {
+    while(true) {
+        if(first == last || first == --last)
+            return;
+        else
+            iter_swap(first++, last);
+    }
+}
+
+template <class RandomIt>
+void __reverse(RandomIt first, RandomIt last,
+               random_access_iterator_tag) {
+    while(first < last)
+        iter_swap(first++, --last);
+}
+
+template <class BiIt>
+inline void reverse(BiIt first, BiIt last) {
+    reverse(first, last, iterator_category_t<BiIt>());
+}
+
+template <class BiIt, class OutputIt>
+OutputIt reverse_copy(BiIt first, BiIt last,
+                      OutputIt result) {
+    while(last != first) {
+        --last;
+        *result = *last;
+        ++result;
+    }
+    return result;
+}
+
+//---------------------
+// rotate and rotate_copy
+
+template <class T>
+T __gcd(T m, T n) {
+    while(n != 0) {
+        T t = m % n;
+        m = n;
+        n = t;
+    }
+    return m;
+}
+
+template <class ForwardIt>
+ForwardIt __rotate(ForwardIt first, ForwardIt mid,
+                   ForwardIt last, forward_iterator_tag) {
+    if(first == mid)
+        return last;
+    if(last == mid)
+        return first;
+
+    ForwardIt first2 = mid;
+    do {
+        swap(*first++, *first2++);
+        if(first == mid)
+            mid = first2;
+    } while(first2 != last);
+
+    ForwardIt new_mid = first;
+    first2 = mid;
+
+    while(first2 != last) {
+        swap(*first++, *first2++);
+        if(first == mid)
+            mid = first2;
+        else if(first2 == last)
+            first2 = mid;
+    }
+
+    return new_mid;
+}
+
+template <class BiIt>
+BiIt __rotate(BiIt first, BiIt mid, BiIt last,
+              bidirectional_iterator_tag) {
+    if(first == mid)
+        return last;
+    if(last == mid)
+        return first;
+
+    __reverse(first, mid, bidirectional_iterator_tag());
+    __reverse(mid, last, bidirectional_iterator_tag());
+
+    while(first != mid && mid != last)
+        swap(*first++, *--last);
+
+    if(first == mid) {
+        __reverse(mid, last, bidirectional_iterator_tag());
+        return last;
+    } else {
+        __reverse(first, mid, bidirectional_iterator_tag());
+        return first;
+    }
+}
+
+template <class RandomIt>
+RandomIt __rotate(RandomIt first, RandomIt mid,
+                  RandomIt last,
+                  random_access_iterator_tag) {
+    using Distance = difference_type_t<RandomIt>;
+    Distance n = last - first;
+    Distance k = mid - first;
+    Distance l = n - k;
+    RandomIt result = first + (last - mid);
+
+    if(k == 0)
+        return last;
+    else if(k == l) {
+        swap_ranges(first, mid, mid);
+        return result;
+    }
+
+    Distance d = gcd(n, k);
+
+    for(Distance i = 0; i < d; i++) {
+        value_type_t<RandomIt> tmp = *first;
+        RandomIt p = first;
+
+        if(k < l) {
+            for (Distance j = 0; j < l/d; j++) {
+                if(p > first + l) {
+                    *p = *(p - l);
+                    p -= l;
+                }
+
+                *p = *(p + k);
+                p += k;
+            }
+        } else {
+            for (Distance j = 0; j < k/d - 1; j ++) {
+                if(p < last - k) {
+                    *p = *(p + k);
+                    p += k;
+                }
+
+                *p = * (p - l);
+                p -= l;
+            }
+        }
+
+        *p = tmp;
+        ++first;
+    }
+    return result;
+}
+
+// rotate return the pos of last element after rorate
+template <class ForwardIt>
+inline ForwardIt rotate(ForwardIt first, ForwardIt mid,
+                        ForwardIt last) {
+    return __rotate(first, mid, last,
+                    iterator_category_t<ForwardIt>());
+}
+
+template <class ForwardIt, class OutputIt>
+OutputIt rotate_copy(ForwardIt first, ForwardIt mid,
+                     ForwardIt last, OutputIt result) {
+    return copy(first, mid, copy(mid, last, result));
 }
 
 
@@ -477,6 +780,20 @@ OutputIt transform(InputIt1 first1, InputIt1 last1,
     for(;first1 != last1; ++first1, ++first2, ++result)
         *result = binary_op(*first1, *first2);
     return result;
+}
+
+// generate and generate_n
+template <class ForwardIt, class Generator>
+void generate(ForwardIt first, ForwardIt last, Generator gen) {
+    for(;first != last;++first)
+        *first = gen();
+}
+
+template <class OutputIt, class Size, class Generator>
+OutputIt generate_n(OutputIt first, Size n, Generator gen) {
+    for(;n > 0;--n, ++first)
+        *first = gen();
+    return first;
 }
 
 } // MiniSTL
