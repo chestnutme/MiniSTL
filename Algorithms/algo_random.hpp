3a9c51f26a45bbaa9e0acde5e13a25bb245b039f
chestnutme <1724759858@qq.com>
2019-02-03 21:37:53 +0800

    done random algos

--- Algorithms/algo_random.hpp
+++ Algorithms/algo_random.hpp
@@ -1,160 +1,83 @@
 #pragma once 
 
+#include "cstdlib"
+
+
 namespace MiniSTL {
 
-// Return a random number in the range [0, __n).  This function encapsulates
+// Return a random number in the range [0, n).  This function encapsulates
 // whether we're using rand (part of the standard C library) or lrand48
 // (not standard, but a much better choice whenever it's available).
 
-template <class _Distance>
-inline _Distance __random_number(_Distance __n) {
-#ifdef __STL_NO_DRAND48
-  return rand() % __n;
-#else
-  return lrand48() % __n;
-#endif
+template <class Distance>
+inline Distance __random_number(Distance n) {
+    return rand() % n;
 }
 
 // random_shuffle
-
-template <class _RandomAccessIter>
-inline void random_shuffle(_RandomAccessIter __first,
-                           _RandomAccessIter __last) {
-  __STL_REQUIRES(_RandomAccessIter, _Mutable_RandomAccessIterator);
-  if (__first == __last) return;
-  for (_RandomAccessIter __i = __first + 1; __i != __last; ++__i)
-    iter_swap(__i, __first + __random_number((__i - __first) + 1));
+template <class RandomIt, class RandomNumberGenerator>
+void random_shuffle(RandomIt first, RandomIt last,
+                    RandomNumberGenerator& rand = __random_number) {
+	if (first == last)
+		return;
+	for(RandomIt i = first + 1; i != last; ++i)
+		iter_swap(i, first + rand((i - first) + 1));
 }
 
-template <class _RandomAccessIter, class _RandomNumberGenerator>
-void random_shuffle(_RandomAccessIter __first, _RandomAccessIter __last,
-                    _RandomNumberGenerator& __rand) {
-  __STL_REQUIRES(_RandomAccessIter, _Mutable_RandomAccessIterator);
-  if (__first == __last) return;
-  for (_RandomAccessIter __i = __first + 1; __i != __last; ++__i)
-    iter_swap(__i, __first + __rand((__i - __first) + 1));
+// random_sample and random_sample_n
+template <class ForwardIt, class OutputIt, class Distance,
+          class RandomNumberGenerator>
+OutputIt random_sample_n(ForwardIt first, ForwardIt last,
+                         OutputIt out, const Distance n,
+                         RandomNumberGenerator& rand = __random_number) {
+	Distance remaining = distance(first, last);
+	Distance m(min(n, remaining));
+
+	while(m > 0) {
+		if (rand(remaining) < m) {
+			*out = *first;
+			++out;
+			--m;
+		}
+
+		--remaining;
+		++first;
+	}
+	return out;
 }
 
-// random_sample and random_sample_n (extensions, not part of the standard).
+template <class InputIt, class RandomIt,
+          class RandomNumberGenerator, class Distance>
+RandomIt random_sample(InputIt first, InputIt last,
+                       RandomIt out, const Distance n,
+					   RandomNumberGenerator& rand = __random_number) {
+	Distance m(0);
+	Distance t(n);
+	for(;first != last && m < n; ++m, ++first)
+		out[m] = *first;
 
-template <class _ForwardIter, class _OutputIter, class _Distance>
-_OutputIter random_sample_n(_ForwardIter __first, _ForwardIter __last,
-                            _OutputIter __out, const _Distance __n)
-{
-  __STL_REQUIRES(_ForwardIter, _ForwardIterator);
-  __STL_REQUIRES(_OutputIter, _OutputIterator);
-  _Distance __remaining = 0;
-  distance(__first, __last, __remaining);
-  _Distance __m = min(__n, __remaining);
+	while(first != last) {
+		++t;
+		Distance M = rand(t);
+		if(M < n)
+			out[M] = *first;
+		++first;
+	}
 
-  while (__m > 0) {
-    if (__random_number(__remaining) < __m) {
-      *__out = *__first;
-      ++__out;
-      --__m;
-    }
-
-    --__remaining;
-    ++__first;
-  }
-  return __out;
+	return out + m;
 }
 
-template <class _ForwardIter, class _OutputIter, class _Distance,
-          class _RandomNumberGenerator>
-_OutputIter random_sample_n(_ForwardIter __first, _ForwardIter __last,
-                            _OutputIter __out, const _Distance __n,
-                            _RandomNumberGenerator& __rand)
-{
-  __STL_REQUIRES(_ForwardIter, _ForwardIterator);
-  __STL_REQUIRES(_OutputIter, _OutputIterator);
-  __STL_UNARY_FUNCTION_CHECK(_RandomNumberGenerator, _Distance, _Distance);
-  _Distance __remaining = 0;
-  distance(__first, __last, __remaining);
-  _Distance __m = min(__n, __remaining);
-
-  while (__m > 0) {
-    if (__rand(__remaining) < __m) {
-      *__out = *__first;
-      ++__out;
-      --__m;
-    }
-
-    --__remaining;
-    ++__first;
-  }
-  return __out;
+template <class InputIt, class RandomIt, 
+          class RandomNumberGenerator>
+inline RandomIt
+random_sample(InputIt first, InputIt last,
+              RandomIt out_first, RandomIt out_last,
+              RandomNumberGenerator& rand = __random_number) {	
+  	return random_sample(first, last,
+                         out_first, 
+                         out_last - out_first,
+						 rand);
 }
 
-template <class _InputIter, class _RandomAccessIter, class _Distance>
-_RandomAccessIter __random_sample(_InputIter __first, _InputIter __last,
-                                  _RandomAccessIter __out,
-                                  const _Distance __n)
-{
-  _Distance __m = 0;
-  _Distance __t = __n;
-  for ( ; __first != __last && __m < __n; ++__m, ++__first) 
-    __out[__m] = *__first;
-
-  while (__first != __last) {
-    ++__t;
-    _Distance __M = __random_number(__t);
-    if (__M < __n)
-      __out[__M] = *__first;
-    ++__first;
-  }
-
-  return __out + __m;
-}
-
-template <class _InputIter, class _RandomAccessIter,
-          class _RandomNumberGenerator, class _Distance>
-_RandomAccessIter __random_sample(_InputIter __first, _InputIter __last,
-                                  _RandomAccessIter __out,
-                                  _RandomNumberGenerator& __rand,
-                                  const _Distance __n)
-{
-  __STL_UNARY_FUNCTION_CHECK(_RandomNumberGenerator, _Distance, _Distance);
-  _Distance __m = 0;
-  _Distance __t = __n;
-  for ( ; __first != __last && __m < __n; ++__m, ++__first)
-    __out[__m] = *__first;
-
-  while (__first != __last) {
-    ++__t;
-    _Distance __M = __rand(__t);
-    if (__M < __n)
-      __out[__M] = *__first;
-    ++__first;
-  }
-
-  return __out + __m;
-}
-
-template <class _InputIter, class _RandomAccessIter>
-inline _RandomAccessIter
-random_sample(_InputIter __first, _InputIter __last,
-              _RandomAccessIter __out_first, _RandomAccessIter __out_last) 
-{
-  __STL_REQUIRES(_InputIter, _InputIterator);
-  __STL_REQUIRES(_RandomAccessIter, _Mutable_RandomAccessIterator);
-  return __random_sample(__first, __last,
-                         __out_first, __out_last - __out_first);
-}
-
-
-template <class _InputIter, class _RandomAccessIter, 
-          class _RandomNumberGenerator>
-inline _RandomAccessIter
-random_sample(_InputIter __first, _InputIter __last,
-              _RandomAccessIter __out_first, _RandomAccessIter __out_last,
-              _RandomNumberGenerator& __rand) 
-{
-  __STL_REQUIRES(_InputIter, _InputIterator);
-  __STL_REQUIRES(_RandomAccessIter, _Mutable_RandomAccessIterator);
-  return __random_sample(__first, __last,
-                         __out_first, __rand,
-                         __out_last - __out_first);
-}
 } // MiniSTL
 
